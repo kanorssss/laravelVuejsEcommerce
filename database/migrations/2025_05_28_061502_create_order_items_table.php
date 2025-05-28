e5bb14a2eece04c03aed5615b2b379f165176210
Airon <pingadairon@gmail.com>
2025-05-28 20:56:37 +0800

    Migrations

--- database/migrations/2025_05_28_061502_create_order_items_table.php
+++ database/migrations/2025_05_28_061502_create_order_items_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignId('order_id')->references('id')->on('orders');
+            $table->foreignId('product_id')->references('id')->on('products');
+            $table->integer('quatity');
+            $table->decimal('unit_price');
         });
     }
 
