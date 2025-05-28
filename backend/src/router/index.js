c115980c963c9d9e97846d06f5c502bce8d28a22
Airon <pingadairon@gmail.com>
2025-05-28 22:20:44 +0800

    Refactor frontend structure and update dependencies
    - Updated index.html to include Tailwind CSS classes for styling.
    - Modified App.vue to use <router-view> for routing.
    - Removed HelloWorld component as part of the refactor.
    - Updated package.json and package-lock.json to include new dependencies: @headlessui/vue, @heroicons/vue, and @tailwindcss/forms.
    - Cleaned up style.css by removing unused styles.

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,15 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import LoginView from "../views/LoginView.vue";
+import DashboardView from "../views/DashboardView.vue";
+import RequestPasswordReset from "../views/RequestPasswordReset.vue";
+import ResetPassword from "../views/ResetPassword.vue";
+
 const routes = [
     {
         path: "/dashboard",
         name: "dashboard",
-        component: Dashboard,
+        component: DashboardView,
     },
     {
         path: "/login",
         name: "login",
-        component: Login,
+        component: LoginView,
+    },
+    {
+        path: "/request-password",
+        name: "requestPassword",
+        component: RequestPasswordReset,
+    },
+    {
+        path: "/reset-password/:token",
+        name: "resetPassword",
+        component: ResetPassword,
     },
 ];
 const router = createRouter({
---
+++ backend/src/views/ResetPassword.vue
@@ -0,0 +1,103 @@
+<template>
+    <!--
+      This example requires updating your template:
+  
+      ```
+      <html class="h-full bg-white">
+      <body class="h-full">
+      ```
+    -->
+    <div
+        class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+    >
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <!-- <img
+                class="mx-auto h-10 w-auto"
+                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Your Company"
+            /> -->
+            <h2
+                class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900"
+            >
+                Set new password
+            </h2>
+        </div>
+
+        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form class="space-y-6" action="#" method="POST">
+                <div>
+                    <label
+                        for="email"
+                        class="block text-sm/6 font-medium text-gray-900"
+                        >New password</label
+                    >
+                    <div class="mt-2">
+                        <input
+                            type="email"
+                            name="email"
+                            id="new-password"
+                            required=""
+                            placeholder="New password"
+                            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                    </div>
+                </div>
+
+                <div>
+                    <!-- <div class="flex items-center justify-between">
+                        <label
+                            for="password"
+                            class="block text-sm/6 font-medium text-gray-900"
+                            >Password</label
+                        >
+                        <div class="text-sm">
+                            <router-link
+                                :to="{ name: 'requestPassword' }"
+                                class="font-semibold text-indigo-600 hover:text-indigo-500"
+                                >Forgot password?</router-link
+                            >
+                        </div>
+                    </div> -->
+                    <label
+                        for="email"
+                        class="block text-sm/6 font-medium text-gray-900"
+                        >Repeat password</label
+                    >
+                    <div class="mt-2">
+                        <input
+                            type="password"
+                            name="password_repeat"
+                            id="password_repeat"
+                            required=""
+                            placeholder="Repeat password"
+                            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <div class="flex items-center justify-between">
+                        <div class="text-sm">
+                            <router-link
+                                :to="{ name: 'login' }"
+                                class="font-semibold text-indigo-600 hover:text-indigo-500"
+                                >Go back to login</router-link
+                            >
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <button
+                        type="submit"
+                        class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                        Sign in
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script setup></script>
+
+<style></style>
