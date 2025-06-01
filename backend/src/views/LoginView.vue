497ba3e1e5103826268f767dc1ec9a8aa9000df0
Airon <pingadairon@gmail.com>
2025-06-01 18:42:25 +0800

    Refactor LoginView button layout and loading state display
    - Updated the button layout in LoginView to improve alignment and spacing.
    - Enhanced loading state display by adjusting the SVG icon position and adding dynamic button text for better user feedback during the sign-in process.

--- backend/src/views/LoginView.vue
+++ backend/src/views/LoginView.vue
@@ -125,38 +125,40 @@ function submit() {
             </div>
 
             <div class="mt-4">
-                <button
-                    :disabled="loading"
-                    type="submit"
-                    :class="{
-                        'cursor-not-allow': loading,
-                        'opacity-50': loading,
-                    }"
-                    class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                    Sign in
-                    <svg
-                        v-if="loading"
-                        class="mr-4 -ml-1 size-5 animate-spin text-white"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
+                <div class="mt-4">
+                    <button
+                        :disabled="loading"
+                        type="submit"
+                        :class="{
+                            'cursor-not-allowed': loading,
+                            'opacity-50': loading,
+                        }"
+                        class="flex w-full justify-center items-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                        <circle
-                            class="opacity-25 mr-10"
-                            cx="12"
-                            cy="12"
-                            r="10"
-                            stroke="currentColor"
-                            stroke-width="4"
-                        ></circle>
-                        <path
-                            class="opacity-75"
-                            fill="currentColor"
-                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                        ></path>
-                    </svg>
-                </button>
+                        <svg
+                            v-if="loading"
+                            class="mr-2 size-5 animate-spin text-white"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <circle
+                                class="opacity-25"
+                                cx="12"
+                                cy="12"
+                                r="10"
+                                stroke="currentColor"
+                                stroke-width="4"
+                            ></circle>
+                            <path
+                                class="opacity-75"
+                                fill="currentColor"
+                                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                            ></path>
+                        </svg>
+                        {{ loading ? "Signing in..." : "Sign in" }}
+                    </button>
+                </div>
             </div>
         </form>
         <GuestLayouts />
