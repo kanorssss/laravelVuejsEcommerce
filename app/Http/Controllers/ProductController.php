7662df8803b811db505fdb4eb0eba8ed158ca7ab
Airon <pingadairon@gmail.com>
2025-06-03 13:36:03 +0800

    - Added ProductController and defined API resource routes for products in the routes file. - Updated DatabaseSeeder to ensure proper formatting by adding a newline at the end of the file.

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductRequest;
+
+use App\Http\Requests\UpdateProductRequest;
+use App\Http\Resources\ProducResource;
+use App\Http\Resources\ProductListResource;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Return a paginated list of products using the ProductListResource that appear frontend
+        return ProductListResource::collection(Product::query()->paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(ProductRequest $request)
+    {
+        // Validate the request and create a new product
+        return new ProducResource(Product::create($request->validated()));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        //        // Return a single product using the ProducResource
+        return new ProducResource($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(ProductRequest $request, Product $product)
+    {
+        // Validate the request and update the product
+        $product->update($request->validated());
+        // Return the updated product using the ProducResource
+        return new ProducResource($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        // Delete the product and return a success message
+        $product->delete();
+        // Return a no content response on successful deletion
+        return response()->noContent();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,19 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'getUser']);
     Route::post('logout', [AuthController::class, 'logout']);
+
+
+    // Define the API resource routes for products
+    // This will automatically create routes for index, store, show, update, and destroy methods
+    Route::apiResource('/products', [ProductController::class]);
 });
 
 Route::post('login', [AuthController::class, 'login']);
